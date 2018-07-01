f05de0346ff1d040d9d4dc3077a76e0067ebbb32
Cole Challand <ctchalland@gmail.com>
2018-07-01 15:42:41 -0500

    Created a variable 'inbox' equaling to async/await new Contract object, deploying it's bytecode and arguments & sending from web3's accounts and gas. Also tested with mocha

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,10 +2,7 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
-const {
-    interface,
-    bytecode
-} = require('../compile');
+const { interface, bytecode } = require('../compile');
 
 let accounts;
 let inbox;
@@ -14,17 +11,10 @@ beforeEach(async () => {
     // Get a list of all accounts
     accounts = await web3.eth.getAccounts();
 
-    // Use one of those accounts to deploy
-    // the contract
+    // Use one of those accounts to deploy the contract
     inbox = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({
-            data: bytecode,
-            arguments: ['Hi there!']
-        })
-        .send({
-            from: accounts[0],
-            gas: '1000000'
-        });
+        .deploy({ data: bytecode, arguments: ['Hi there!'] })
+        .send({ from: accounts[0], gas: '1000000'});
 });
 
 describe('Inbox', () => {
