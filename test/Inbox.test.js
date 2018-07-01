972d44381f43146562f5cb568706ef37484608f3
Cole Challand <ctchalland@gmail.com>
2018-07-01 17:01:59 -0500

    Refactored to be more dymaic for testing Contract's arguments

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,11 +1,13 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 const { interface, bytecode } = require('../compile');
 
 let accounts;
 let inbox;
+const INITIAL_STRING = 'Hi there!';
 
 beforeEach(async () => {
     // Get a list of all accounts
@@ -13,7 +15,7 @@ beforeEach(async () => {
 
     // Use one of those accounts to deploy the contract
     inbox = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({ data: bytecode, arguments: ['Hi there!'] })
+        .deploy({ data: bytecode, arguments: [INITIAL_STRING] })
         .send({ from: accounts[0], gas: '1000000'});
 });
 
@@ -24,7 +26,7 @@ describe('Inbox', () => {
 
     it('has a default message', async () => {
         const message = await inbox.methods.message().call();
-        assert.equal(message, 'Hi there!');
+        assert.equal(message, INITIAL_STRING);
     });
 
     it('can change the message', async () => {
